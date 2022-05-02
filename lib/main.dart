007ecc006b0c833b96557ad3eafe4d997d3dda60
FlutLab.io <>
2022-05-02 19:12:34 +0000

    Autosave

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/material.dart';
+import 'package:castillo/gridviewarticulos.dart';
 
 void main() {
   runApp(MyApp());
@@ -17,54 +17,3 @@ class MyApp extends StatelessWidget {
         home: MyHomePage());
   }
 } //Widget sin estado
-
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key? key}) : super(key: key);
-  @override
-  _MyHomePageState createState() => _MyHomePageState();
-} //Widgets con estado
-
-class _MyHomePageState extends State<MyHomePage> {
-  List<String> images = [
-    "assets/images/alimentador.png",
-    "assets/images/taza.png",
-    "assets/images/tazon.png",
-    "assets/images/agua.png",
-    "assets/images/cepillo.png",
-    "assets/images/esponja.png",
-    "assets/images/alimentador.png",
-    "assets/images/taza.png",
-    "assets/images/tazon.png",
-    "assets/images/alimentador.png",
-    "assets/images/taza.png",
-    "assets/images/tazon.png",
-    "assets/images/alimentador.png",
-    "assets/images/taza.png",
-    "assets/images/tazon.png",
-    "assets/images/alimentador.png",
-    "assets/images/taza.png",
-    "assets/images/tazon.png",
-    "assets/images/alimentador.png",
-    "assets/images/taza.png",
-    "assets/images/tazon.png",
-    "assets/images/alimentador.png",
-    "assets/images/taza.png",
-    "assets/images/tazon.png",
-  ];
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-        appBar: AppBar(
-          title: Text("Flutter GridView"),
-        ),
-        body: GridView.builder(
-          itemCount: images.length,
-          itemBuilder: (BuildContext context, int index) {
-            return Image.asset(images[index], fit: BoxFit.cover);
-          },
-          gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(crossAxisCount: 3, mainAxisSpacing: 10, crossAxisSpacing: 10),
-          padding: EdgeInsets.all(10),
-          shrinkWrap: true,
-        ));
-  }
-}
